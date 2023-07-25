4d13077337ab39299d70817cddfc593151dcf959
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-07-25 16:12:55 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -11,14 +11,37 @@
 
 // console.log("Now you're ready to go outside!");
 
-const temperature = -1;
+//______________________________
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// const temperature = -1;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+//______________________________
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18){
+//   console.log("You are eligible to vote");
+// }
+
+// let temperature = 41;
+
+// if (temperature < -40 || temperature > 40){
+//   console.log("Maybe going outside isn't such a good idea...");
+// };
+
+
+var raining = false;
+if (!raining){
+  console.log("Leave your umbrella at home!");
 }
-
-console.log("Now you're ready to go outside!");
