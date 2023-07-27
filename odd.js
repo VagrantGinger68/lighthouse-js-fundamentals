9bd2704a79c4e3a540dbbdb3633773f34084fac5
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-07-27 19:31:19 -0400

    Made a function to test if a number is Odd

---
+++ odd.js
@@ -0,0 +1,11 @@
+function isOdd(num){
+  if(num % 2 !== 0){
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("11 is odd: " + isOdd(11));
