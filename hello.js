b3f64abf9f2b6bc7deece15f995a6c5a4236e60c
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-02 11:34:29 -0600

    Added return vs log functions

--- hello.js
+++ hello.js
@@ -1,5 +1,16 @@
-const sayHello = function (name) {
-  console.log("Hello, " + name);
-}
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// }
 
-sayHello("Ryan");
+// sayHello("Ryan");
+
+// const sayHelloToConsole = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " +  name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
